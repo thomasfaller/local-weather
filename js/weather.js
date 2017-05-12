a4dcea75b25570e52f65cc15c397c0b32131dd29
Thomas <thomasfaller@gmail.com>
2017-05-12 22:02:20 +0100

    icon

--- js/weather.js
+++ js/weather.js
@@ -10,7 +10,13 @@ $( document ).ready(function() {               // LOADS THE FOLLOWING CODE WHEN 
   navigator.geolocation.getCurrentPosition(function(position) {   // HTML Geolocation API
     var $lat = position.coords.latitude;
     var $long = position.coords.longitude;
-    $.ajax({url: "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + $lat + "&lon=" + $long + "&APPID=bae4b37551ad15c5be94bb6d7178601d", success: function(result){   // retrieve the JSON
+        $.ajax({
+
+            url: "http://api.openweathermap.org/data/2.5/weather?lat=" + $lat + "&lon=" + $long + "&APPID=bae4b37551ad15c5be94bb6d7178601d",
+            type: "GET",
+            dataType: "jsonp",
+            success: function(result){   // retrieve the JSON
+
           var $city = result.name;                                // City
           var $country = result.sys.country;                      // Country
           var $weatherDesc = result.weather[0].description;       // Description of Weather
@@ -34,7 +40,7 @@ $( document ).ready(function() {               // LOADS THE FOLLOWING CODE WHEN 
           // DISPLAYING THE INFO IN THE WEB APP
 
           $locationWrapper.html("<h1>" + $city + ", " + $country + "</h1>");
-          $weatherIconWrapper.html("<img style=\"width: 120px;\" src=\"..Files/" + result.weather[0].main + "-icon.png\" alt=\"weather icon\">");
+          // $weatherIconWrapper.html("<img style=\"width: 120px;\" src=\"..Files/Rain-icon.ico\" alt=\"weather icon\">");
           $weatherStatusWrapper.html("<h1>" + $weatherDesc + "</h1>");
           $tempWrapper.html("<h1>" + $tempC.toFixed(0) + " °C</h1>");     // toFixed(0) to make sure the temp is a whole number
           // $(".wrapper").css();
