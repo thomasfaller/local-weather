2628d343985f4b9fe0acf4e8a458e87d0743d4bf
Thomas <thomasfaller@gmail.com>
2017-05-15 14:35:43 +0100

    Geolocation done through API

--- js/weather.js
+++ js/weather.js
@@ -5,25 +5,36 @@ var $tempWrapper = $("#temp-wrapper");
 var $locationWrapper = $("#location");
 var $weatherIconWrapper = $("#weather-icon-wrapper");
 var $weatherStatusWrapper = $("#weather-status");
+var $minTempWrapper = $("#min_temp");
+var $maxTempWrapper = $("#max_temp");
 
-$( document ).ready(function() {               // LOADS THE FOLLOWING CODE WHEN THE PAGE IS LOADED
-  navigator.geolocation.getCurrentPosition(function(position) {   // HTML Geolocation API
-    var $lat = position.coords.latitude;
-    var $long = position.coords.longitude;
+// Geolocation
+
+var $lat;
+var $long;
+$.ajax({
+  url: "http://ip-api.com/json",
+  success: function(georesult) {
+    $lat = georesult.lat;
+    $long = georesult.lon;
+
         $.ajax({
 
             url: "http://api.openweathermap.org/data/2.5/weather?lat=" + $lat + "&lon=" + $long + "&APPID=bae4b37551ad15c5be94bb6d7178601d",
-            type: "GET",
-            dataType: "jsonp",
             success: function(result){   // retrieve the JSON
-
           var $city = result.name;                                // City
           var $country = result.sys.country;                      // Country
           var $weatherDesc = result.weather[0].description;       // Description of Weather
           var $weatherIcon = result.weather[0].icon;              // Icon code from openweathermap
+
+// TEMPERATURES VARIABLES
+
           var $tempC = result.main.temp - 273.15;                 // Temp in Celsius
           var $tempF = result.main.temp * (9/5) - 459.67;         // Temp in Farenheit
-
+          var $tempMinC = result.main.temp_min - 273.15;          // Min Temp in Celsius
+          var $tempMinF = result.main.temp_min * (9/5) - 459.67;  // Min Temp in Farenheit
+          var $tempMaxC = result.main.temp_max - 273.15;          // Max Temp in Celsius
+          var $tempMaxF = result.main.temp_max * (9/5) - 459.67;  // Max Temp in Farenheit
 
 
           // if (result.weather[0].main === "Rain") {
@@ -44,9 +55,9 @@ $( document ).ready(function() {               // LOADS THE FOLLOWING CODE WHEN 
           $weatherStatusWrapper.html("<h1>" + $weatherDesc + "</h1>");
           $tempWrapper.html("<h1>" + $tempC.toFixed(0) + " °C</h1>");     // toFixed(0) to make sure the temp is a whole number
           // $(".wrapper").css();
-
+          // $maxTempWrapper.html("<h3>" + $tempMaxC + "°C</h3>");
 
 
         }});
-  });
-      });
+
+        }});
